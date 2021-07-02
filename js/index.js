8f5279c70229ffb02136a18d9fd528715ba2961e
Liliia Novikova <kplily85@gmail.com>
2021-07-02 10:13:56 +0300

    fixed burger menu

--- js/index.js
+++ js/index.js
@@ -1,23 +1,18 @@
 import refs from "./refs.js";
 
-refs.btnBackPage.addEventListener("click", OnBackPage);
-refs.btnFirstPage.addEventListener("click", OnfirstPage);
-refs.btnLastPage.addEventListener("click", OnLastPage);
-refs.btnToPage.addEventListener("click", OnToPage);
-refs.iconMenu.addEventListener("click", onBurgerMenu);
-refs.tableContainer.addEventListener("click", tableSort);
+refs.iconMenu.addEventListener("click", onBurgerMenuClick);
 
 // burger menu
-
-function onBurgerMenu() {
+function onBurgerMenuClick() {
+  console.log("click");
   document.body.classList.toggle("lock");
   refs.iconMenu.classList.toggle("active");
   refs.nav.classList.toggle("active");
-  onMenu();
+  onNavClick();
 }
 
-function onMenu() {
+function onNavClick() {
   refs.navLink.forEach((elem) => {
-    elem.addEventListener("click", () => onBurgerMenu());
+    elem.addEventListener("click", () => onBurgerMenuClick());
   });
 }
